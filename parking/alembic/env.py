675de96105cc3a2c8e29670e90d2c68615c3e598
vikneo <vm-viktor@mail.ru>
2025-05-05 21:35:44 +0700

    check with isort env.py

--- parking/alembic/env.py
+++ parking/alembic/env.py
@@ -1,8 +1,9 @@
 from logging.config import fileConfig
 
 from alembic import context
-from ..models import db
 from sqlalchemy import engine_from_config, pool
+
+from ..models import db
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
