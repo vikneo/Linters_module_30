41e65cada92274acc6e369606efdb165afd84571
vikneo <vm-viktor@mail.ru>
2025-05-05 20:28:15 +0700

    configuring alembics and creating migrations

--- parking/alembic/env.py
+++ parking/alembic/env.py
@@ -18,7 +18,10 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+from models import db
+
+target_metadata = db.metadata
+
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -40,10 +43,10 @@ def run_migrations_offline() -> None:
     """
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
-        url=url,
-        target_metadata=target_metadata,
-        literal_binds=True,
-        dialect_opts={"paramstyle": "named"},
+        url = url,
+        target_metadata = target_metadata,
+        literal_binds = True,
+        dialect_opts = {"paramstyle": "named"},
     )
 
     with context.begin_transaction():
@@ -59,13 +62,13 @@ def run_migrations_online() -> None:
     """
     connectable = engine_from_config(
         config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
+        prefix = "sqlalchemy.",
+        poolclass = pool.NullPool,
     )
 
     with connectable.connect() as connection:
         context.configure(
-            connection=connection, target_metadata=target_metadata
+            connection = connection, target_metadata = target_metadata
         )
 
         with context.begin_transaction():
