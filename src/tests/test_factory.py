4c33d77c0a9e6517d2253e1e8135f93a79f05804
vikneo <vm-viktor@mail.ru>
2025-05-08 17:02:34 +0700

    fixing pytest errors in test_factory.py

--- src/tests/test_factory.py
+++ src/tests/test_factory.py
@@ -25,7 +25,7 @@ def test_create_parking(client, db):
     db.session.commit()
     now_parkings = db.session.query(Parking).all()
 
-    assert now_parkings.id == 3
+    assert now_parkings[-1].id == parking.id
     assert parking.name == now_parkings[-1].name
     assert len(then_parkings) < len(now_parkings)
     assert len(now_parkings) == 3
